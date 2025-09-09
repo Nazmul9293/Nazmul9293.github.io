c23606ae1958d5505ca6674f45274391049b233e
Nazmul9293 <nazmul.fig@gmail.com>
2025-09-09 02:41:40 +0000

    Deploying to gh-pages from @ Nazmul9293/Nazmul9293.github.io@dca26d8c7dcc8c9ce216e94b18f15e11e0f4e7a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,14 +474,49 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%61%7A%6D%75%6C.%37%35%39%32%39%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/md-nazmul-islam-b51478265", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/nazmul9293", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile//", "_blank");
         },
       },{
         id: 'social-rss',
@@ -491,18 +526,25 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-journal_publication',
+        title: 'Journal_publication',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://doi.org/10.1016/j.hybadv.2023.100090", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-contact_form',
+        title: 'Contact_form',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://formspree.io/f/xblaknpg", "_blank");
+        },
+      },{
+        id: 'social-personal_website',
+        title: 'Personal_website',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://nazmul9293.github.io", "_blank");
         },
       },{
       id: 'light-theme',
